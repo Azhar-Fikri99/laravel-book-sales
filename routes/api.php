39166c9289897b47af0621c0d54c29b155200577
Azhar_Fikri <azharfikri889@gmail.com>
2025-01-16 13:36:14 +0700

    feat: merapikan api

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Api\PaymentMethodController;
 use App\Http\Controllers\Api\PaymentController;
 use App\Http\Controllers\Api\PaymentsController;
 use App\Http\Resources\OrderResource;
+use Database\Seeders\PaymentMethodSeeder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,15 +25,15 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
 
 
-Route::apiResource('/books', BookController::class);
-Route::apiResource('/genres', GenreController::class);
+Route::apiResource('/books', BookController::class)->only(['index', 'show']);
+Route::apiResource('/genres', GenreController::class)->only(['index', 'show']);
+Route::apiResource('/authors', AuthorController::class)->only(['index', 'show']);
 Route::apiResource('/orders', OrderController::class);
-Route::apiResource('/authors', AuthorController::class);
-Route::apiResource('/payment_methods', PaymentMethodController::class);
+
 
 
 // ini wajib login middle war atas ini
-Route::middleware(['auth:api'])->group(function (){
+Route::middleware(['auth:api'])->group(function () {
 
     // ini get info (harus login dulu)
 
@@ -49,6 +50,11 @@ Route::middleware(['auth:api'])->group(function (){
     // ini kalau kita mau ambil id
     // Route::get('/user', fn(Request $request) =>  $request->user()->id);
 
+    Route::apiResource('/orders', OrderController::class)->only(['index', 'show','store']);
+    Route::apiResource('/payment_methods', PaymentMethodController::class)->only(['index', 'show']);
+
+
+    ///===================================
     Route::middleware(['role:admin,staff'])->group(function (){
         // Route::post('/books', [BookController::class, 'store']);
         // Route::post('/genres', [GenreController::class, 'store']);
@@ -59,65 +65,75 @@ Route::middleware(['auth:api'])->group(function (){
         Route::apiResource('/genres', GenreController::class)->only(['store', 'update','destroy']);
         Route::apiResource('/authors', AuthorController::class)->only(['store','update', 'destory']);
 
+
+        Route::apiResource('/orders', OrderController::class)->only(['update', 'destory']);
+
+
+        Route::apiResource('/payment', PaymentController::class)->only(['update', 'destroy']);
+        Route::apiResource('/payment_methods', PaymentMethodSeeder::class)->only(['update', 'destroy']);
+
+
         // Route::apiResource('/payment_methods', PaymentMethodController::class)->only(['store','update','destroy']);
 
 
     });
 
+});
+
     // Route::apiResource('/payment_methods', PaymentMethodController::class)->only(['index', 'show']);
 
 
-    //tugas 2 Januari 2025//crud admin staff
-    // customer read aja
-    Route::middleware(['role:customer'])->group(function (){
+//     //tugas 2 Januari 2025//crud admin staff
+//     // customer read aja
+//     Route::middleware(['role:customer'])->group(function (){
 
-    });
-
-
-
-
-    //    payments
-   Route::middleware(['role:customer'])->group(function (){
-    Route::apiResource('/payment', PaymentController::class)->only(['index', 'show', "store"]);
-    });
-
-
-   Route::middleware(['role:staff'])->group(function (){
-// strore create
-    Route::apiResource('/payment', PaymentController::class)->only(["update"]);
-    });
-
-    Route::middleware(['role:admin'])->group(function (){
-        // strore create
-        Route::apiResource('/payments', PaymentController::class)->only(["destroy"]);
-    });
-
-});
-
-//======================================================================================
+//     });
 
 
 
 
-Route::middleware(['role:customer'])->group(function (){
-    Route::apiResource('/orders', OrderController::class);
-});
+//     //    payments
+//    Route::middleware(['role:customer'])->group(function (){
+//     Route::apiResource('/payment', PaymentController::class)->only(['index', 'show', "store"]);
+//     });
+
+
+//    Route::middleware(['role:staff'])->group(function (){
+// // strore create
+//     Route::apiResource('/payment', PaymentController::class)->only(["update"]);
+//     });
+
+//     Route::middleware(['role:admin'])->group(function (){
+//         // strore create
+//         Route::apiResource('/payments', PaymentController::class)->only(["destroy"]);
+//     });
+
+// });
+
+// //======================================================================================
 
 
 
-//ini yang bisa di akses oleh orang lain
-Route::apiResource('/books', BookController::class)->only(['index', 'show']);
-Route::apiResource('/genres', GenreController::class)->only(['index', 'show']);
-Route::apiResource('/authors', AuthorController::class)->only(['index', 'show']);
+
+// Route::middleware(['role:customer'])->group(function (){
+//     Route::apiResource('/orders', OrderController::class);
+// });
+
+
+
+// //ini yang bisa di akses oleh orang lain
+// Route::apiResource('/books', BookController::class)->only(['index', 'show']);
+// Route::apiResource('/genres', GenreController::class)->only(['index', 'show']);
+// Route::apiResource('/authors', AuthorController::class)->only(['index', 'show']);
 
 
 
 
 
-Route::apiResource('/books', BookController::class);
-Route::apiResource('/genres', GenreController::class);
-Route::apiResource('/orders', OrderController::class);
-Route::apiResource('/payment_methods', PaymentMethodController::class);
+// Route::apiResource('/books', BookController::class);
+// Route::apiResource('/genres', GenreController::class);
+// Route::apiResource('/orders', OrderController::class);
+// Route::apiResource('/payment_methods', PaymentMethodController::class);
 
 
 
